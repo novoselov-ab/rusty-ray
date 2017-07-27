9e7605e9ebb1d81d975b93c58370d352e8530849
Anton Novoselov <novoselov.ab@gmail.com>
2017-07-27 08:57:12 +0300

    switch to glium

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,69 @@
-extern crate gl;
-extern crate glutin;
-extern crate libc;
+extern crate rand;
 
-use glutin::GlContext;
+#[macro_use]
+extern crate glium;
+extern crate image;
+
+use std::io::Cursor;
+use glium::{glutin, Surface};
+
+mod support;
 
 fn main() {
+    // Building the display, ie. the main object
     let mut events_loop = glutin::EventsLoop::new();
-    let window = glutin::WindowBuilder::new()
-        .with_title("Hello world!")
-        .with_dimensions(1024, 768);
-    let context = glutin::ContextBuilder::new()
-        .with_vsync(true);
-    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
+    let window = glutin::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new().with_vsync(true);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    unsafe {
-        gl_window.make_current().unwrap();
-    }
+    // building a texture with "OpenGL" drawn on it
+    let image = image::load(Cursor::new(&include_bytes!("opengl.png")[..]),
+                            image::PNG).unwrap().to_rgba();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let opengl_texture = glium::Texture2d::new(&display, image).unwrap();
 
-    unsafe {
-        gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
-        gl::ClearColor(0.0, 1.0, 0.0, 1.0);
-    }
+    // building a 1024x1024 empty texture
+    let dest_texture = glium::Texture2d::empty_with_format(&display,
+                                               glium::texture::UncompressedFloatFormat::U8U8U8U8,
+                                               glium::texture::MipmapsOption::NoMipmap,
+                                               1024, 1024).unwrap();
+    dest_texture.as_surface().clear_color(0.0, 0.0, 0.0, 1.0);
 
-    let mut running = true;
-    while running {
+    // the main loop
+    support::start_loop(|| {
+        // we have one out of 60 chances to blit one `opengl_texture` over `dest_texture`
+        if rand::random::<f64>() <= 0.016666 {
+            let (left, bottom, dimensions): (f32, f32, f32) = rand::random();
+            let dest_rect = glium::BlitTarget {
+                left: (left * dest_texture.get_width() as f32) as u32,
+                bottom: (bottom * dest_texture.get_height().unwrap() as f32) as u32,
+                width: (dimensions * dest_texture.get_width() as f32) as i32,
+                height: (dimensions * dest_texture.get_height().unwrap() as f32) as i32,
+            };
+
+            opengl_texture.as_surface().blit_whole_color_to(&dest_texture.as_surface(), &dest_rect,
+                                                            glium::uniforms::MagnifySamplerFilter::Linear);
+        }
+
+        // drawing a frame
+        let target = display.draw();
+        dest_texture.as_surface().fill(&target, glium::uniforms::MagnifySamplerFilter::Linear);
+        target.finish().unwrap();
+
+        let mut action = support::Action::Continue;
+
+        // polling and handling the events received by the window
         events_loop.poll_events(|event| {
             match event {
-                glutin::Event::WindowEvent{ event, .. } => match event {
-                    glutin::WindowEvent::Closed => running = false,
-                    glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
-                    _ => ()
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::Closed => action = support::Action::Stop,
+                    _ => (),
                 },
-                _ => ()
+                _ => (),
             }
         });
 
-        unsafe {
-            gl::Clear(gl::COLOR_BUFFER_BIT);
-        }
-
-        gl_window.swap_buffers().unwrap();
-    }
+        action
+    });
 }
